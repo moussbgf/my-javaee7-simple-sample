8639c0716d908985f049092ab9b132fafbea625e
moussbgf DTA <mboughazif@gmail.com>
2017-04-10 14:17:55 +0200

    8e commit

--- src/main/java/org/javaee7/sample/MyApplication.java
+++ src/main/java/org/javaee7/sample/MyApplication.java
@@ -6,4 +6,8 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("/resources")
 public class MyApplication extends Application {
 
+    //ajout d'un commentaire
+
+    //encore un commentaire
+
 }
